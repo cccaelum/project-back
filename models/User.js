1c58efc38040d9607d6ec04ed0d13ad98657839a
cccaelum <ccc.trad@gmail.com>
2024-12-05 09:02:22 +0100

    userModelUpdate

--- models/User.js
+++ models/User.js
@@ -4,12 +4,7 @@ const userSchema = new mongoose.Schema({
   uid: { type: String, required: true, unique: true }, // UID único de Firebase
   name: String,
   email: String,
-  posts: [
-    {
-      content: String,
-      timestamp: { type: Date, default: Date.now },
-    },
-  ],
+  city: String,
 });
 
 module.exports = mongoose.model("User", userSchema);
